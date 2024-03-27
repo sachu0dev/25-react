ff83c9c08c825cd2329ba6c6bb5104e8266fdda1
sachu0dev <sushil.dev.in@gmail.com>
2024-03-27 22:22:10 +0530

    accordian

--- 25-react-projects/src/components/accordian/index.jsx
+++ 25-react-projects/src/components/accordian/index.jsx
@@ -6,12 +6,30 @@ import data from "./data";
 // MULTIPLE SELECTION
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
+  function handleMultiSelection(getCurrentId) {
+    let tempMultiple = [...multiple];
+    const findIndexOfCurrentId = tempMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) {
+      tempMultiple.push(getCurrentId);
+    } else {
+      tempMultiple.splice(findIndexOfCurrentId, 1);
+    }
+
+    setMultiple(tempMultiple);
+  }
+
   return (
     <div className="wrapper flex flex-col mt-[20vh] items-center h-full">
-      <button className="mb-4 bg-red-500 p-2 rounded-md text-white font-bold">
+      <button
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        className="mb-4 bg-red-500 p-2 rounded-md text-white font-bold"
+      >
         Enable Multi Selection
       </button>
       <div className="accordian w-[40vw] flex flex-col justify-center items-center">
@@ -24,12 +42,17 @@ const Accordian = () => {
               >
                 <div
                   className="title flex w-full justify-between text-2xl p-4 bg-blue-700 text-white"
-                  onClick={() => handleSingleSelection(dataItem.id)}
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }
                 >
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
-                {selected === dataItem.id ? (
+                {selected === dataItem.id ||
+                multiple.indexOf(dataItem.id) !== -1 ? (
                   <div className="p-4 text-xl text-white bg-blue-800">
                     {dataItem.answer}
                   </div>
